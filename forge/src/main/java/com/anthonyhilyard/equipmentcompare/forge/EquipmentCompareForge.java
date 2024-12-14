19c9c76877c24403ce58204c254675176fc60b94
Anthony Hilyard <anthony.hilyard@gmail.com>
2024-12-14 12:38:24 -0800

    Fixed Forge loading issue.

--- forge/src/main/java/com/anthonyhilyard/equipmentcompare/forge/EquipmentCompareForge.java
+++ forge/src/main/java/com/anthonyhilyard/equipmentcompare/forge/EquipmentCompareForge.java
@@ -3,13 +3,13 @@ package com.anthonyhilyard.equipmentcompare.forge;
 import com.anthonyhilyard.equipmentcompare.EquipmentCompare;
 
 import net.minecraftforge.fml.IExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(EquipmentCompare.MODID)
 public final class EquipmentCompareForge
 {
-	public EquipmentCompareForge(ModLoadingContext context)
+	public EquipmentCompareForge(FMLJavaModLoadingContext context)
 	{
 		context.registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> "ANY", (remote, isServer) -> true));
 	}
